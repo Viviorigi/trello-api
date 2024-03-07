adfc9fd5e954564359f51c7939baf694f2e5f008
Viviorigi <duong682@gmail.com>
2024-03-07 11:14:13 +0700

    api create board

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,6 +1,8 @@
 
 import Joi from 'joi'
-
+import { ObjectId } from 'mongodb'
+import { GET_DB } from '~/config/mongodb'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validator'
 //Define collections (name &schema)
 
 const BOARD_COLLECTION_NAME = 'boards'
@@ -10,14 +12,38 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
 
-  cardOrderIds: Joi.array().items(Joi.string()).default([]),
+
+  columnOrderIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+  ).default([]),
 
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
+const createNew = async (data) => {
+  try {
+    const valiData = await validateBeforeCreate(data)
+    // console.log(valiData)
+    const createBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(valiData)
+    return createBoard
+  } catch (error) {throw new Error(error)}
+}
+
+const findOneById = async (id) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(id) })
+    return result
+  } catch (error) {throw new Error(error)}
+}
 export const boardModel = {
   BOARD_COLLECTION_NAME,
-  BOARD_COLLECTION_SCHEMA
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById
 }
