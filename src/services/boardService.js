bd2652a1627a8fd4b19cb46b3efab3688746428c
Viviorigi <duong682@gmail.com>
2024-03-08 10:52:30 +0700

    aggregate

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -4,7 +4,7 @@ import { slugify } from '~/utils/formaters'
 import { boardModel } from '~/models/boardModel'
 import ApiError from '~/utils/ApiError'
 import { StatusCodes } from 'http-status-codes'
-
+import { cloneDeep } from 'lodash'
 const createNew = async (reqBody) => {
   try {
     // xu ly logic du lieu tuy dac thu
@@ -30,7 +30,21 @@ const getDetails = async (boardId) => {
 
     const board = await boardModel.getDetails(boardId)
     if (!board) {throw new ApiError(StatusCodes.NOT_FOUND, 'Board NOT FOUND!')}
-    return board
+
+    // deep clone board tao ra mot cai moi de xu ly khong anh huong toi board ban dau tuy muc dich ma co clone deep hay khong
+    const resBoard = cloneDeep(board)
+    //Dua card ve dung column cua no
+
+    resBoard.columns.forEach(column => {
+      //cach 1
+      // column.cards = resBoard.cards.filter(card => card.columnId.toString() === column._id.toString())
+
+      //cach 2
+      column.cards = resBoard.cards.filter(card => card.columnId.equals(column._id))
+    })
+    delete resBoard.cards
+
+    return resBoard
   } catch (error) {throw error}
 
 }
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,6 +1,7 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
+import { BOARD_TYPES } from '~/utils/constants'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
@@ -11,7 +12,8 @@ const createNew = async (req, res, next) => {
       'string.max':'Title max 50 chars',
       'string.trim':'Tittle muse not ave leading or trailing whitespace'
     }),
-    description: Joi.string().required().min(3).max(256).trim().strict()
+    description: Joi.string().required().min(3).max(256).trim().strict(),
+    type: Joi.string().valid(...Object.values(BOARD_TYPES)).required()
   })
 
   try {
