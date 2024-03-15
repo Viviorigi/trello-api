ad7057cdb2f606e7731801b0f68ff8f908f4fe55
Viviorigi <duong682@gmail.com>
2024-03-15 20:41:57 +0700

    ok

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 //nhung domain dc phep truy cap den tai nguyen server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
+  'https://trello-web-virid-nu.vercel.app/'
 ]
 
 export const BOARD_TYPES ={
